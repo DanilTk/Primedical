de4d696d69487d7ab28e8ee8aac3bcf5be79a5a2
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2021-02-17 22:49:40 +0100

    Controller added

---
+++ src/main/java/pl/med/demo/controllers/ScreeningController.java
@@ -0,0 +1,25 @@
+package pl.med.demo.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.med.demo.model.Prescription;
+import pl.med.demo.model.UserProfile;
+import pl.med.demo.service.ScreeningService;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping(value = "/api/v1/screenings")
+@RequiredArgsConstructor
+public class ScreeningController {
+    private final ScreeningService screeningService;
+
+    @GetMapping(value = "/all")
+    ResponseEntity<Set<Prescription>> conductFullScreening(UserProfile userProfile) {
+        return ResponseEntity.status(HttpStatus.OK).body(screeningService.screenForPrescriptions(userProfile));
+    }
+}

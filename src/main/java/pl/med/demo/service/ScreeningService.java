8bd8a2d673c6152a600e0d4eaeaac2aef58fd6e0
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2021-02-17 22:29:33 +0100

    Screening Service added

--- src/main/java/pl/med/demo/service/ScreeningService.java
+++ src/main/java/pl/med/demo/service/ScreeningService.java
@@ -1,19 +1,31 @@
 package pl.med.demo.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.med.demo.model.Prescription;
 import pl.med.demo.model.UserProfile;
+import pl.med.demo.service.screening_programs.CholesterolScreening;
+import pl.med.demo.service.screening_programs.DiabetesScreening;
+import pl.med.demo.service.screening_programs.HypertensionScreening;
 
 import java.util.Set;
-
-/**
- * Service aggregates all available screenings within one
- */
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ScreeningService {
+    private final DiabetesScreening diabetesScreening;
+    private final CholesterolScreening cholesterolScreening;
+    private final HypertensionScreening hypertensionScreening;
 
     public Set<Prescription> screenForPrescriptions(UserProfile userProfile) {
-        return null;
+        Prescription diabetesPrescription = diabetesScreening.performScreening(userProfile);
+        Prescription cholesterolPrescription = cholesterolScreening.performScreening(userProfile);
+        Prescription hypertensionPrescription = hypertensionScreening.performScreening(userProfile);
+
+        return Set.of(diabetesPrescription, cholesterolPrescription, hypertensionPrescription)
+                .stream()
+                .filter(prescription -> !prescription.isHealthy())
+                .collect(Collectors.toSet());
     }
 }

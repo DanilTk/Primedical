05a4dee02dd0a1053f3e86ca5893ee2d47b805a0
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2021-02-20 21:50:59 +0100

    Basic test added

--- src/main/java/pl/med/demo/model/ScreeningResult.java
+++ src/main/java/pl/med/demo/model/ScreeningResult.java
@@ -1,10 +1,14 @@
 package pl.med.demo.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 import java.util.Set;
 
+@Getter
+@Setter
 @AllArgsConstructor
 public class ScreeningResult {
     private Set<Prescription> prescriptions;
--- src/test/java/pl/med/demo/DemoApplicationTests.java
+++ src/test/java/pl/med/demo/DemoApplicationTests.java
@@ -1,14 +1,38 @@
 package pl.med.demo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.med.demo.model.*;
+import pl.med.demo.service.ScreeningService;
+
+import java.util.Collections;
+import java.util.Set;
 
 @SpringBootTest
 class DemoApplicationTests {
+    @Autowired
+    ScreeningService screeningService;
 
     @Test
-    void contextLoads() {
-        UserQuestionnaire questionnaire = UserQuestionnaire.builder()
+    @DisplayName("Should return correct result with prescriptions")
+    void conductScreening() {
+
+        //given:
+        UserQuestionnaire questionnaire = buildQuestionnaire();
+
+        //when:
+        ScreeningResult result = screeningService.conductScreening(questionnaire);
+
+        //then:
+        Assertions.assertEquals(0, result.getExceptionMessages().size());
+        Assertions.assertEquals(3, result.getPrescriptions().size());
+    }
+
+    private UserQuestionnaire buildQuestionnaire() {
+        return UserQuestionnaire.builder()
                 .gender(Gender.M)
                 .height(183)
                 .weight(85)
@@ -16,8 +40,7 @@ class DemoApplicationTests {
                 .smokingQuestionnaire(new SmokingQuestionnaire(false, 0, 0, 0))
                 .activityHours(5)
                 .conditions(Set.of(new Condition(ConditionName.CVD, null)))
-                .familyCondition(Collections.emptySet())
+                .familyConditions(Collections.emptySet())
                 .build();
-
-
+    }
 }
